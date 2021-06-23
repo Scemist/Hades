99b4c46cce3e58b6d8ff1479323cc1268100b056
Lucas Scemist <scemist.lucas@gmail.com>
2021-06-23 17:11:11 -0300

    Criação do design e CSS da página

--- index.php
+++ index.php
@@ -5,29 +5,50 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="root.css">
+        <link rel="stylesheet" href="header.css">
+        <link rel="stylesheet" href="main.css">
 
         <title>HADES</title>
     </head>
 
     <body>
-        <main>
-            <h1>Hades_</h1>        
-
-            <div>
-                <label>Passphrase 1_</label>
-                <input type="password">
-                <hr>
-
-                <label>Passphrase 2_</label>
-                <input type="password" name="passphrase2">
-                <hr>
-
-                <input id="requisicao" type="submit" value="Atualizar">
+        <header>
+            <h1 id="hades">Hades_</h1>        
+            
+            <div class="input-grupo">
+                <label>Passphrase 1</label>
+                <input class="caixa" type="password">
             </div>
+            
+            <div class="input-grupo">
+                <label>Passphrase 2</label>
+                <input class="caixa" type="password" name="passphrase2">
+            </div>
+            
+            <input id="requisicao" type="submit" value="ATUALIZAR">
+            
+        </header>
 
-            <div id="conteudo"></div>
+        <main>
+            <div class="card card-head">
+                <h3 id="dark">Assim falou Zaratustra</h3>
+            </div>
+            <div  class="card">
+                <p contenteditable="true">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit <br><br>
+
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor aliquam minima suscipit doloremque repellat iste a, dignissimos qui perferendis corrupti rerum cumque aperiam nisi. In animi omnis eos aspernatur officia ea aliquid? Hic odio rerum placeat ipsam soluta voluptatum omnis a quidem excepturi reiciendis asperiores, voluptatibus quae itaque doloremque? Ullam dicta reprehenderit adipisci consequuntur deleniti itaque quo maiores nostrum sunt. Eos nam quo eveniet eius consequuntur, laboriosam pariatur accusantium nisi? Laborum, quia eum. <br><br>
+                
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga non maiores dolorem labore. Iste ab voluptate iure dolorum veritatis repellat, cumque repudiandae, similique facilis illum quaerat maxime, nesciunt at odio? Aspernatur saepe molestias eius eos distinctio repellendus necessitatibus voluptatum, fugiat magni? Commodi iusto adipisci, voluptate odio cum impedit voluptates nihil atque itaque, tenetur dolorum cupiditate aperiam, vel nisi! Provident necessitatibus odio nesciunt quibusdam tempore! Aliquam impedit quod vero voluptas corrupti deleniti in? Sed nemo beatae in deserunt ipsa iure consequatur voluptates, exercitationem eum nulla veniam officiis nihil cum porro quisquam molestiae, mollitia corporis velit. Modi voluptas amet illo eaque quidem sequi deserunt quo quia, ipsum rerum quasi asperiores, est accusantium excepturi quibusdam. Error dolorem maxime, consequuntur omnis, quisquam dicta aliquid deleniti perspiciatis neque doloremque debitis exercitationem repudiandae, commodi animi laudantium.
+</p>
+            </div>
+            <div class="card card-foot">
+                <input id="salvar" type="submit" value="SALVAR">
+            </div>
         </main>
-
-        <script src="funcoes/slip.js"></script>
+            
+            <script src="funcoes/slip.js"></script>
+            <script src="app.js"></script>
     </body>
 </html>
