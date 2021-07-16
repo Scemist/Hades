6b9460dc659819433ef74105859e6b7c44c25b15
Lucas Scemist <scemist.lucas@gmail.com>
2021-07-16 19:21:02 -0300

    Implementada função Ajax para criar novo arquivo
    Uma função Ajax de XMLHttpRequest de Javascript para PHP salva o arquivo no servidor. O sistema também lista os documentos existentes

--- php/arquivo.php
+++ php/arquivo.php
@@ -1,25 +1,59 @@
 <?php
 
-    $corpo = $_POST['corpo'];
-    $titulo = $_POST['titulo'];
+    // $corpo = $_POST['corpo'];
+    // $titulo = $_POST['titulo'];
 
-    $instancia = new arquivo();
-    $instancia -> criar($corpo, $titulo);
+    // $instancia = new arquivo();
+    // $instancia -> criar($corpo, $titulo);
+
+    if ($_POST):
+
+        switch ($_POST['tipo']):
+            case 'criar':
+                
+                $instancia = new arquivo();
+                $instancia -> criar($_POST['titulo']);
+            break;
+
+            case 'salvar':
+                echo 'salvar';
+            break;
+            
+            default:
+                # code...
+            break;
+        endswitch;
+    endif;
 
     class arquivo {
 
         public $nome = 'arquivo';
         public $titulo;
         public $corpo;
-        
-        function criar($corpo, $titulo) {
 
-            $this -> corpo = $corpo;
+        function listar() {
+            
+            $diretorio = 'documentos/';
+            $arquivos = scandir($diretorio);
+            $arquivos = array_diff($arquivos, array('..', '.'));
+
+            function pegarNome($arquivo) {
+
+                return pathinfo($arquivo, PATHINFO_FILENAME);
+            }
+
+            return $arquivos = array_map('pegarNome', $arquivos);
+        }
+        
+        function criar($titulo) {
+
             $this -> titulo = $titulo;
 
-            $fopen = fopen('../documentos/' . $this -> titulo . ".txt", 'wb');
-            fwrite($fopen, $corpo);
-            fclose($fopen);
+            $arquivo = fopen('../documentos/' . $this -> titulo . ".txt", 'wb');
+            fwrite($arquivo, $this -> titulo);
+            fclose($arquivo);
+
+            echo 'Sucesso';
         }
         
         function editar() {
