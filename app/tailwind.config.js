b4dd2aeed257350fbda10b96a0e56dbadc249edb
Lucas Scemist <scemist.lucas@gmail.com>
2023-07-18 16:35:18 -0300

    npm run dev for Waching Changes and UI Improved

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,10 +1,12 @@
 console.log(process.env.NODE_ENV)
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	// safelist: process.env.NODE_ENV === 'development' ? [{ pattern: /./, }] : [],
-	safelist: [{ pattern: /./, }],
-	// content: ["./static/css/*.css", "./templates/*.jinja", "./templates/*.html"],
+	// safelist: [{ pattern: /.*/, }],
+	content: [
+		"./static/css/tailwind.css",
+		"./templates/*.jinja",
+		"./templates/*.html",
+	],
 	theme: {
 		extend: {},
 	},
